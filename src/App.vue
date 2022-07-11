0de802c0e80879a0e8af5b923fb75f2202e67eb4
Vedant Singhania <vedant.singhania@gmail.com>
2022-07-10 19:44:54 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import { ref } from 'vue'
+import json from './data.json'
+
+const data = ref(json)
+
+</script>
+
+
+
+<template>
+<div v-for='item in data'>
+  <p>{{item.title}}</p>
+  <p>{{item.messageTitle}}: {{item.message}}</p>
+
+  <input :type="item.type" :id="item.type" v-model="item.message" :placeholder="item.placeholder">
+  <label v-if="item.label != null">{{item.label}}</label>
+  <br>
+  <br>
+
+</div>
+
+</template>
